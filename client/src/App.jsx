100faddc2ef0f8cc3cb7707074720c9bc7eea8d5
Harshit2N <harshitsinghshakya23@gmail.com>
2025-09-27 23:45:01 +0530

    added background image and paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 const App = () => {
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="w-full min-h-screen bg-[url('./src/assets/bgImage.svg')] bg-cover bg-center">
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import SideBar from '../components/SideBar'
+import ChatContainer from '../components/ChatContainer'
+import RightSideBar from '../components/RightSideBar'
 const HomePage = () => {
+  const [selectedUser,setSeletedUser]=useState(false);
   return (
-    <div>
-      <h1>Home page</h1>
+    <div className='border w-full min-h-screen sm:px-[15%] sm:py-[5%] bg-cover bg-center'>
+      <div className='backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid gird-cols-1 relative'>
+        <SideBar></SideBar>
+        <ChatContainer></ChatContainer>
+        <RightSideBar></RightSideBar>
+      </div>
     </div>
   )
 }
