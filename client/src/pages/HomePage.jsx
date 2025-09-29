f148831b2c8786fb2fd2a63dcd7fed3b2ca00505
Harshit2N <harshitsinghshakya23@gmail.com>
2025-09-29 22:54:53 +0530

    completed home Page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,13 +3,13 @@ import SideBar from '../components/SideBar'
 import ChatContainer from '../components/ChatContainer'
 import RightSideBar from '../components/RightSideBar'
 const HomePage = () => {
-  const [selectedUser,setSeletedUser]=useState(false);
+  const [selectedUser,setSelectedUser]=useState(false);
   return (
-    <div className='border w-full min-h-screen sm:px-[15%] sm:py-[5%] bg-cover bg-center'>
-      <div className='backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid gird-cols-1 relative'>
-        <SideBar></SideBar>
-        <ChatContainer></ChatContainer>
-        <RightSideBar></RightSideBar>
+    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%] bg-cover bg-center'>
+      <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser?'md:grid-cols-[1fr_1.5fr_1.5fr] xl:grid-cols-[1fr_2fr_1fr]':'md:grid-cols-2'}`}>
+        <SideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser}></SideBar>
+        <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser}></ChatContainer>
+        <RightSideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser}></RightSideBar>
       </div>
     </div>
   )
