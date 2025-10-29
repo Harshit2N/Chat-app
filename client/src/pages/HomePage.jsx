fb7463892785617df3dc2f3e6be246b4f736c5af
Harshit2N <harshitsinghshakya23@gmail.com>
2025-10-29 19:49:01 +0530

    Online Users fucntionality

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%] bg-cover bg-center'>
       <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser?'md:grid-cols-[1fr_1.5fr_1.5fr] xl:grid-cols-[1fr_2fr_1fr]':'md:grid-cols-2'}`}>
-        <SideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser}></SideBar>
+        <SideBar></SideBar>
         <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser}></ChatContainer>
         <RightSideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser}></RightSideBar>
       </div>
